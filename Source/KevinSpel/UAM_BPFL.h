1a178e15ca87dbf09b8f8d5cc1ed6b2939e9be71
Mythical-Github <MythicalData@gmail.com>
2024-08-13 04:21:41 -0400

    cleanup

---
+++ Source/KevinSpel/UAM_BPFL.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UAM_BPFL.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KEVINSPEL_API UUAM_BPFL : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
